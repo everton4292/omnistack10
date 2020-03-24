18bf873cac43060fb965f22ea0d62c11b64a1ca6
everton4292 <ecoresende@hotmail.com>
2020-03-23 22:48:54 -0300

    implemented socket.io

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
     async index(request, response) {
@@ -12,7 +13,7 @@ module.exports = {
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
         let dev = await Dev.findOne({ github_username });
-        if (!dev) {
+        if (dev === null) {
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
             const { name = login, avatar_url, bio } = apiResponse.data;
             const techsArray = parseStringAsArray(techs);
@@ -28,6 +29,13 @@ module.exports = {
                 techs: techsArray,
                 location,
             })
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            )
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
 
         }
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,23 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
+const http = require('http')
+const cors = require ('cors');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://everton:34773132@cluster0-gl5nx.mongodb.net/omnistack10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 
 
-app.listen(3333);
+server.listen(3333);
