4d142300f79d3bf99f6ca2986cddbd9907f66694
everton4292 <ecoresende@hotmail.com>
2020-03-23 22:48:00 -0300

    implemented socket.io

--- mobile/src/pages/main.js
+++ mobile/src/pages/main.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 import { MaterialIcons } from '@expo/vector-icons';
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 
 function Main({ navigation }) {
@@ -35,6 +36,22 @@ function Main({ navigation }) {
         loadInitialPosition();
     }, []);
 
+    useEffect(() => {
+        subscribeToNewDevs(dev => setDevs([...devs, dev]));
+    }, [devs])
+
+    function setupWebSocket() {
+        disconnect();
+        
+        const {latitude, longitude} = currentRegion;
+        connect(
+            latitude,
+            longitude,
+            techs,
+        );
+        
+    }
+
     async function loadDevs() {
         const { latitude, longitude } = currentRegion;
         const response = await api.get('/search', {
@@ -46,11 +63,11 @@ function Main({ navigation }) {
         });
 
         setDevs(response.data.devs);
+        setupWebSocket();
     }
 
     function handleRegionChanged(region) {
-        setCurrentRegion(region);
-
+        
     }
 
 
